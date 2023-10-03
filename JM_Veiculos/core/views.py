2f90238751b5af3af3f97a2c849e080ff4422c04
Luiz Henrique Carneiro Carvalho <111710021+Dom-Luiz-III@users.noreply.github.com>
2023-10-03 10:16:47 -0300

    Update views.py

--- JM_Veiculos/core/views.py
+++ JM_Veiculos/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.shortcuts import get_object_or_404
 
 from django.http import HttpResponse
@@ -8,6 +8,8 @@ from .models import Carro
 from .models import Moto
 from .models import Utilitarios
 
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
 def index(request):
     carros = Carro.objects.all()
@@ -25,7 +27,7 @@ def index(request):
 
 
 def moto(request):
-    motos = Moto.objects.all()
+    motos = Moto.objects.filter(situacao='venda')
 
     context = {
         'motos': motos
@@ -33,18 +35,41 @@ def moto(request):
 
     return render(request, 'moto.html', context)
 
+
 def carro(request):
-    carros = Carro.objects.all()
+    carros = Carro.objects.filter(situacao='venda')
 
     context = {
         'carros': carros,
     }
     return render(request, 'carro.html', context)
 
+
 def utilitarios(request):
-    utilitarios = Utilitarios.objects.all()
+    utilitarios = Utilitarios.objects.filter(situacao='venda')
 
     context = {
         'utilitarios': utilitarios,
     }
     return render(request, 'utilitarios.html', context)
+
+
+# Acões para realizar a compra de veículos:
+def comprar_veiculo(request, veiculo_tipo, veiculo_id):
+    if veiculo_tipo == 'carro':
+        veiculo = get_object_or_404(Carro, pk=veiculo_id)
+    elif veiculo_tipo == 'moto':
+        veiculo = get_object_or_404(Moto, pk=veiculo_id)
+    elif veiculo_tipo == 'utilitarios':
+        veiculo = get_object_or_404(Utilitarios, pk=veiculo_id)
+    else:
+        # Aqui serve pra fazer excessões se os ifs não forem aceitos, caso tenham alguma ideia podem modificar ele aqui
+        pass
+
+    if veiculo.situacao == 'venda':
+        veiculo.situacao = 'vendido'
+        veiculo.save()
+        return redirect(f'{veiculo_tipo}')
+    else:
+        # Aqui ele vai renderizar um pop up quando um veículo estiver indisponível usando JavaScript
+        return render(request, f'{veiculo_tipo}.html', {'veiculo': veiculo, 'veiculo_indisponivel': True})
