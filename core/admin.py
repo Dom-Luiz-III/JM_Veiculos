a4b6220f15c4fc44cba59642816677864f7bb7fb
Luiz Henrique Carneiro Carvalho <111710021+Dom-Luiz-III@users.noreply.github.com>
2023-10-18 10:17:03 -0300

    alterando a página admin

--- core/admin.py
+++ core/admin.py
@@ -13,6 +13,13 @@ class CarroAdmin(admin.ModelAdmin):
 
     display_foto.short_description = 'Foto'
 
+    list_filter = ('marca', 'estado')
+
+    search_fields = ['modelo', 'marca', 'ano_fabricacao', 'estado', 'km_rodados',
+                    'leilao', 'formas_pagamento', 'situacao', 'placa']
+    
+    list_per_page = 10
+
 
 class MotoAdmin(admin.ModelAdmin):
     list_display = ('modelo', 'marca', 'ano_fabricacao', 'estado', 'km_rodados',
@@ -22,6 +29,13 @@ class MotoAdmin(admin.ModelAdmin):
         return format_html('<img src="{}" width="150" height="100" style="border-radius: 10px" />', obj.foto.url)
 
     display_foto.short_description = 'Foto'
+
+    list_filter = ('marca', 'estado')
+
+    search_fields = ['modelo', 'marca', 'ano_fabricacao', 'estado', 'km_rodados',
+                    'leilao', 'formas_pagamento', 'situacao', 'placa']
+    
+    list_per_page = 10
 
 
 class UtilitariosAdmin(admin.ModelAdmin):
@@ -33,10 +47,21 @@ class UtilitariosAdmin(admin.ModelAdmin):
 
     display_foto.short_description = 'Foto'
 
+    list_filter = ('marca', 'estado')
+
+    search_fields = ['modelo', 'marca', 'ano_fabricacao', 'estado', 'km_rodados',
+                    'leilao', 'formas_pagamento', 'situacao', 'placa']
+    
+    list_per_page = 10
 
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'email', 'telefone', 'mensagem') 
-    list_filter = ('nome', 'email') 
+
+    list_filter= ('mensagem',)
+
+    search_fields = ['nome', 'email', 'telefone']
+
+    list_per_page = 10
 
 admin.site.register(Carro, CarroAdmin)
 admin.site.register(Moto, MotoAdmin)
