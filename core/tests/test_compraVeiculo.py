93cc62c3c067e893d3a4053725095375f5d5b096
Vinicius Gomes Lobo <120745001+ViniciusGLS2004@users.noreply.github.com>
2023-11-24 09:19:51 -0300

    Update test_compraVeiculo.py

--- core/tests/test_compraVeiculo.py
+++ core/tests/test_compraVeiculo.py
@@ -10,26 +10,50 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+
 class TestCompraVeiculo():
-  def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
-  
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_compraVeiculo(self):
-    self.driver.get("https://luizhenrique.pythonanywhere.com/home")
-    self.driver.set_window_size(1382, 784)
-    self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler").click()
-    self.driver.find_element(By.LINK_TEXT, "Carros").click()
-    self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .btn").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
-    self.driver.find_element(By.LINK_TEXT, "Motos").click()
-    self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .btn").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
-    self.driver.find_element(By.LINK_TEXT, "Utilitários").click()
-    self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .btn").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".navbar-brand").click()
-    self.driver.close()
-  
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+
+    def teardown_method(self, method):
+        self.driver.quit()
+
+    def test_compraVeiculo(self):
+        self.driver.get("https://luizhenrique.pythonanywhere.com/home")
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.set_window_size(1382, 784)
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler").click()
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.find_element(By.LINK_TEXT, "Carros").click()
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .btn").click()
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.find_element(By.LINK_TEXT, "Motos").click()
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .btn").click()
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.find_element(By.CSS_SELECTOR, ".navbar-toggler-icon").click()
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.find_element(By.LINK_TEXT, "Utilitários").click()
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .btn").click()
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.find_element(By.CSS_SELECTOR, ".navbar-brand").click()
+        time.sleep(3)  # Adiciona uma pausa de 3 segundos
+
+        self.driver.close()
